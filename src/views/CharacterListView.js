c3f9b9f7741ddfd949cbf831993f9085036ecc8d
ohheyimjen <jendority@gmail.com>
2019-04-17 15:14:51 -0700

    First draft of CharacterListView.js with major bugs

--- src/views/CharacterListView.js
+++ src/views/CharacterListView.js
@@ -3,14 +3,24 @@ import { connect } from "react-redux";
 
 import { CharacterList } from "../components";
 // import actions
+import { getCharacter } from "../actions";
 
 class CharacterListView extends React.Component {
   constructor() {
     super();
+    this.state = {
+      characters: ''
+    };
   }
 
   componentDidMount() {
     // call our action
+    const starWarsCharacter = props => {
+      const fetchCharacter = e => {
+        e.preventDefault();
+        props.getCharacter();
+      }
+    }
   }
 
   render() {
@@ -27,9 +37,14 @@ class CharacterListView extends React.Component {
 
 // our mapStateToProps needs to have two properties inherited from state
 // the characters and the fetching boolean
+
+const mapStateToProps = state = ({
+  characters: state.characters,
+  error: state.error
+})
 export default connect(
-  null /* mapStateToProps replaces null here */,
+  mapStateToProps,
   {
-    /* action creators go here */
+    getCharacter
   }
 )(CharacterListView);
