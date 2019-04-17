a4048adbce98d85ad4943034f93e8b65abeb8f5f
ohheyimjen <jendority@gmail.com>
2019-04-17 14:20:53 -0700

    Late to commit changes. Completed first drafts of actions, reducers and index

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,8 +1,26 @@
 // we'll need axios
+import { axios } from 'axios';
 
 // we'll need to create 3 different action types here.
 // one for fetching, one for success and one for failure
+export const FETCH_CHARACTER_LIST = 'FETCH_CHARACTER_LIST';
+export const SUCCESS = 'SUCCESS';
+export const FAILURE = 'FAILURE';
+
+export const getCharacter= () => dispatch => {
+    dispatch({type: FETCH_CHARACTER_LIST});
+
+    axios.get('https://swapi.co/api/people/')
+    .then(res => dispatch ({
+        type: SUCCESS,
+        payload: res.data
+    }))
+    .catch(err => dispatch ({
+        type: FAILURE,
+        payload: 'Fetch the characters, you can not'
+    }))
+}
 
 // our action creator will be a function that returns a function
 // the url to fetch characters from is `https://swapi.co/api/people/`
-// remember that now we have controll over our thunk-based action creator
+// remember that now we have control over our thunk-based action creator
--- src/reducers/starWarsReducer.js
+++ src/reducers/starWarsReducer.js
@@ -1,10 +1,34 @@
 import /* we need our action types here*/ "../actions";
+import { FETCH_CHARACTER_LIST, SUCCESS, FAILURE } from "../actions";
+
 const initialState = {
-  characters: []
+  characters: [],
+  error: null,
+  isLoading: false
   // Array characters, Boolean fetching, null error.
 };
 export const charsReducer = (state = initialState, action) => {
   switch (action.type) {
+    case FETCH_CHARACTER_LIST:
+    return {
+      ...state,
+      error: '',
+      isLoading: true
+    }
+
+    case SUCCESS:
+    return {
+      ...state,
+      error: '',
+      characters: action.payload
+    }
+
+    case FAILURE: 
+    return {
+      ...state,
+      isLoading: false,
+      error: action.payload
+    }
     // Fill me in with the important reducers
     // action types should be FETCHING, SUCCESS and FAILURE
     // your switch statement should handle all of these cases.
